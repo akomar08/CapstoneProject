b89d7871b22a0cba45de3f30cab5248517182957
akomar08 <AJKomar@student.naperville203.org>
2016-04-13 11:39:53 -0500

    4/13/16
    Made Point class and started arrayCreator class

---
+++ Data Analysis/Point.java
@@ -0,0 +1,23 @@
+
+
+/**
+ * A data point.
+ * 
+ * @author Alex Komar 
+ * @version 4/13/16
+ */
+public class Point
+{
+    /** description of instance variable x (add comment for each instance variable) */
+    private double x;
+    private double y;
+
+    /**
+     * Default constructor for objects of class Point
+     */
+    public Point(double x,double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
